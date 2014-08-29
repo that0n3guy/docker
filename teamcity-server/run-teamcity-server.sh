febff7090d595b3ccd6172a31c1db827b1bed180
Peter O <that0n3guy@users.noreply.github.com>
2014-08-29 16:51:49 -0500

    changing away from start script

--- teamcity-server/run-teamcity-server.sh
+++ teamcity-server/run-teamcity-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec ${TEAM_CITY_INSTALL_DIR}TeamCity/bin/teamcity-server.sh start >>/var/log/teamcity-server.log 2>&1
+exec ${TEAM_CITY_INSTALL_DIR}TeamCity/bin/teamcity-server.sh run >>/var/log/teamcity-server.log 2>&1
