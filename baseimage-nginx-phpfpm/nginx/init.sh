438f190a13264651716a7156bc7ddfe8848aae3e
Peter O <that0n3guy@users.noreply.github.com>
2015-02-23 15:19:32 -0600

    fixed nginx error if default vhost file is not there, and typos

--- baseimage-nginx-phpfpm/nginx/init.sh
+++ baseimage-nginx-phpfpm/nginx/init.sh
@@ -7,27 +7,29 @@ then
     NGINX_DOCROOT="$DOCROOT"
 fi
 
-sed -i "s#root .*;#root $NGINX_DOCROOT;#" /etc/nginx/sites-enabled/default
+DEFAULT_FILE="/etc/nginx/sites-enabled/default"
+if [ -f DEFAULT_FILE ]; then
+    sed -i "s#root .*;#root $NGINX_DOCROOT;#" /etc/nginx/sites-enabled/default
+fi
+
 mkdir -p $NGINX_DOCROOT
 
-if [ -z ${SYNC_UID+x} ]; then
-    if [ $SYNC_UID -eq 1 ];
+if [ $SYNC_UID -eq 1 ]
+then
+    uid=`stat -c '%u' $DOCROOT`
+    gid=`stat -c '%g' $DOCROOT`
+
+    echo "Updating www-data ids to ($uid:$gid)"
+
+    if [ ! $uid -eq 0 ]
     then
-        uid=`stat -c '%u' $DOCROOT`
-        gid=`stat -c '%g' $DOCROOT`
-
-        echo "Updating www-data ids to ($uid:$gid)"
-
-        if [ ! $uid -eq 0 ]
-        then
-            sed -i "s#^www-data:x:.*:.*:#www-data:x:$uid:$gid:#" /etc/passwd
-        fi
-
-        if [ ! $gid -eq 0 ]
-        then
-            sed -i "s#^www-data:x:.*:#www-data:x:$gid:#" /etc/group
-        fi
-
-        chown www-data /var/log/nginx
+        sed -i "s#^www-data:x:.*:.*:#www-data:x:$uid:$gid:#" /etc/passwd
     fi
+
+    if [ ! $gid -eq 0 ]
+    then
+        sed -i "s#^www-data:x:.*:#www-data:x:$gid:#" /etc/group
+    fi
+
+    chown www-data /var/log/nginx
 fi
