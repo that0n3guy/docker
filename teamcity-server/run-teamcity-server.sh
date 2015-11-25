218830f580ad4a10e261082b108ddc467c5d07e0
Peter O <that0n3guy@users.noreply.github.com>
2015-11-25 16:06:29 -0600

    changed how jdbc driver is downloaded

--- teamcity-server/run-teamcity-server.sh
+++ teamcity-server/run-teamcity-server.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# move the jdbc to the correct location if its not already their.
+if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/$MYSQL_JDBC_PACKAGE-bin.jar" ];
+then
+  mv /opt/$MYSQL_JDBC_PACKAGE/$MYSQL_JDBC_PACKAGE-bin.jar $TEAMCITY_DATA_PATH/lib/jdbc
+fi
 exec ${TEAM_CITY_INSTALL_DIR}TeamCity/bin/teamcity-server.sh run >>/var/log/teamcity-server.log 2>&1
