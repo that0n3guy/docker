728f624241cbdee40a7e83c9ac089bb0f5fc8bee
that0n3guy <polson@oncalladvisors.com>
2015-11-26 15:46:09 -0600

    teamcity server mysql folder check

--- teamcity-server/run-teamcity-server.sh
+++ teamcity-server/run-teamcity-server.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # move the jdbc to the correct location
-#if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/$MYSQL_JDBC_PACKAGE-bin.jar" ];
-#then
-  mv /opt/$MYSQL_JDBC_PACKAGE/$MYSQL_JDBC_PACKAGE-bin.jar $TEAMCITY_DATA_PATH/lib/jdbc
-#fi
+if [ ! -d "$TEAMCITY_DATA_PATH/lib/jdbc" ];
+then
+  mkdir -p $TEAMCITY_DATA_PATH/lib/jdbc
+fi
+mv /opt/$MYSQL_JDBC_PACKAGE/$MYSQL_JDBC_PACKAGE-bin.jar $TEAMCITY_DATA_PATH/lib/jdbc
 exec ${TEAM_CITY_INSTALL_DIR}TeamCity/bin/teamcity-server.sh run >>/var/log/teamcity-server.log 2>&1
