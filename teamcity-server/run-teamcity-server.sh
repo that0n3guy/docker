dcf58705914666c9df0539918ea08fbe9280bca6
that0n3guy <polson@oncalladvisors.com>
2015-11-26 10:16:23 -0600

    change so always copy jdbc so its updated on dockerfile update

--- teamcity-server/run-teamcity-server.sh
+++ teamcity-server/run-teamcity-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# move the jdbc to the correct location if its not already their.
-if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/$MYSQL_JDBC_PACKAGE-bin.jar" ];
-then
+# move the jdbc to the correct location
+#if [ ! -f "$TEAMCITY_DATA_PATH/lib/jdbc/$MYSQL_JDBC_PACKAGE-bin.jar" ];
+#then
   mv /opt/$MYSQL_JDBC_PACKAGE/$MYSQL_JDBC_PACKAGE-bin.jar $TEAMCITY_DATA_PATH/lib/jdbc
-fi
+#fi
 exec ${TEAM_CITY_INSTALL_DIR}TeamCity/bin/teamcity-server.sh run >>/var/log/teamcity-server.log 2>&1
