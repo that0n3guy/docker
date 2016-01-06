faad808ea9a89b8bae4a97cc6c5b7b212909cb37
that0n3guy <polson+github@oncalladvisors.com>
2016-01-06 16:36:15 -0600

    Added a check for jdbc package.

--- teamcity-server/run-teamcity-server.sh
+++ teamcity-server/run-teamcity-server.sh
@@ -4,5 +4,8 @@ if [ ! -d "$TEAMCITY_DATA_PATH/lib/jdbc" ];
 then
   mkdir -p $TEAMCITY_DATA_PATH/lib/jdbc
 fi
-mv /opt/$MYSQL_JDBC_PACKAGE/$MYSQL_JDBC_PACKAGE-bin.jar $TEAMCITY_DATA_PATH/lib/jdbc
+if [ -f /tmp/foo.txt ]; then
+  # if file jdbc package doesn't exist, the container was probably restarted and this is skipped
+  mv /opt/$MYSQL_JDBC_PACKAGE/$MYSQL_JDBC_PACKAGE-bin.jar $TEAMCITY_DATA_PATH/lib/jdbc
+fi
 exec ${TEAM_CITY_INSTALL_DIR}TeamCity/bin/teamcity-server.sh run >>/var/log/teamcity-server.log 2>&1
